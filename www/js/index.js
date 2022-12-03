bc2890d31ba5fc56a5c8b1410ab16ef4242e1785
Arrieta <albert.arrieta@uleth.ca>
2022-12-02 17:27:09 -0700

    more stuff

--- www/js/index.js
+++ www/js/index.js
@@ -2,5 +2,17 @@ var app = new Framework7({
     // App root element
     el: '#app',
     // ... other parameters
+
+    routes: [
+        {
+          path: '/',
+          url: 'index.html',
+        },
+        {
+          path: '/help/',
+          url: 'pages/help.html',
+          scripts: 'js/script.js/'
+        },
+    ]
   });
 var mainView = app.views.create('.view-main')
